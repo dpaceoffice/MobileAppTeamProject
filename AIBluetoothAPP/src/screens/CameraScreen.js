5d18608ceb0d2805086242f79205b917b42f7cde
Andy T <73973372+Dekaid@users.noreply.github.com>
2021-12-02 13:00:07 -0600

    added basic search,camera, and bluetooth screen

---
+++ AIBluetoothAPP/src/screens/CameraScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View, StyleSheet, Button, FlatList, TouchableOpacity, ScrollView} from "react-native";
+
+
+const CameraScreen = (props) => {
+
+    return <View>
+        <Text>Camera Screen</Text>
+    </View>
+}
+
+
+const styles = StyleSheet.create({
+    
+});
+
+export default CameraScreen;
---
+++ AIBluetoothAPP/src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View, StyleSheet, Button, FlatList, TouchableOpacity, ScrollView} from "react-native";
+
+const SearchScreen = (props) => {
+
+    return <View>
+        <Text>Search Screen</Text>
+    </View>
+}
+
+
+const styles = StyleSheet.create({
+    
+});
+
+export default SearchScreen;
