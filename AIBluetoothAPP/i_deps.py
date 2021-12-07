0eb9e16a1493dabe37149be72788e5f7a9c48234
dpaceoffice <dpaceoffice@gmail.com>
2021-12-07 00:00:06 -0600

    oops#2

--- AIBluetoothAPP/i_deps.py
+++ AIBluetoothAPP/i_deps.py
@@ -4,4 +4,4 @@ import os
 with open('./package.json') as file:
     data = json.load(file)
     for dep in data['dependencies']:
-        os.system("npm i "+data)
+        os.system("npm i "+str(data))
